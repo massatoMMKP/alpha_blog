02210b2b56c22fbf2995f337036f4bd3341b7bad
massatoMMKP <massato.kanenoo@gmail.com>
2025-12-04 19:56:56 -0300

    Add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root 'pages#home'
-  get 'about', to: 'pages#about'
+  root "pages#home"
+  get "about", to: "pages#about"
   resources :articles
+  get "signup", to: "users#new"
+  resources:users
+  resources :users, except: [:new]
 end
