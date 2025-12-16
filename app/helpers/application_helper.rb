0fee2118bf761bf61526e77fcbe08e4d63fb4e10
massatoMMKP <massato.kanenoo@gmail.com>
2025-12-15 23:14:51 -0300

    Add users show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def gravatar_for(user, options = { size: 80 })
+    # Assume you manually set the email_address here or get it from user input
+    email_address = user.email.downcase
+    
+    
+    hash = Digest::MD5.hexdigest(email_address)
+
+    size = options[:size]
+
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "gravatar", width: size, height: size, class: "rounded shadow mx-auto d-block")
+  end
 end
