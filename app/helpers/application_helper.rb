695485b973aaa59bba52a5ac75fa082a2eabb248
massatoMMKP <massato.kanenoo@gmail.com>
2026-01-06 16:29:13 -0300

    Add sessions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,16 @@ module ApplicationHelper
     gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: "gravatar", width: size, height: size, class: "rounded shadow mx-auto d-block")
   end
+  
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+      session[:user_id] = nil unless @current_user
+    end
+  end
+
+
+  def logged_in?
+    !!current_user
+  end
 end
