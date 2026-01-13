9d27af549ae1415b02cb261e12de419503a99b76
massatoMMKP <massato.kanenoo@gmail.com>
2026-01-12 22:36:32 -0300

    Add test

---
+++ test/models/categories_test.rb
@@ -0,0 +1,30 @@
+class CategoriesTest < ActiveSupport::TestCase
+
+  def setup
+    @category = Category.new(name: "Test Category")
+  end
+  test "categories should be valid" do
+    assert @category.valid?
+  end
+
+  test "category name should be present" do
+    @category.name = " "
+    assert_not @category.valid?
+  end
+
+  test "category name should be unique" do
+    @category.save
+    @category2 = Category.new(name: "Test Category")
+    assert_not @category2.valid?
+  end
+
+  test "category name should not be too long" do
+    @category.name = "a" * 26
+    assert_not @category.valid?
+  end
+
+  test "category name should not be too short" do
+    @category.name = "aa"
+    assert_not @category.valid?
+  end
+end
