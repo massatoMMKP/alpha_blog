5dac1389f97a72cc1129283f8e45be63e5d03e06
massatoMMKP <massato.kanenoo@gmail.com>
2026-01-13 23:38:47 -0300

    Add integration_test, but i don't finshed yet

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,17 @@
 class CategoriesController < ApplicationController
   def new 
+    @category = Category.new
+
+  end
+
+  def create 
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was created successfully."
+      redirect_to category_path(@category)
+    else
+      render 'new', status: :unprocessable_entity
+    end
 
   end
 
@@ -8,7 +20,12 @@ class CategoriesController < ApplicationController
   end
 
   def show 
-    
+    @category = Category.find(params[:id])
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
   end
   
 end
